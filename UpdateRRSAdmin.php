719cebb0a8daf69dab91239e6b998d12e1675dce
KanekoHato <1672019@maranatha.ac.id>
2021-10-07 10:17:45 +0700

    Otomatis export rrs ke json
    Otomatis export rrs ke json

--- UpdateRRSAdmin.php
+++ UpdateRRSAdmin.php
@@ -30,12 +30,13 @@ class Berita
 $tribunObj = new Berita();
 $rssData = $tribunObj->GetRss();
 $xml = json_decode(json_encode(simplexml_load_string($rssData, null, LIBXML_NOCDATA)), true);
-
+$fp = fopen('berita.json', 'w');
 foreach ($xml['channel']['item'] as $data_berita) {
     $judul = $data_berita['title'];
     $deskripsi = $data_berita['description'];
     $url_berita = $data_berita['link'];
     $tanggal = $data_berita['pubDate'];
+    fwrite($fp, json_encode($xml, JSON_PRETTY_PRINT));
 
     $stmt = $dbh->prepare('INSERT INTO berita (judul,deskripsi,link,tanggal) VALUES(?,?,?,?)');  
     $stmt->execute([$judul,$deskripsi,$url_berita,$tanggal]);  
@@ -43,6 +44,20 @@ foreach ($xml['channel']['item'] as $data_berita) {
 
 
 echo 'Sukses Mengupdate Berita';
+
+
+
+
+
+//json_enconde($array, $options(optional) is the method to convert array into JSON
+
+
+//close the file
+fclose($fp);
+
+
+
+
 header('location: admin.php');
 
 
