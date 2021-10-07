9174da9cfabdc3e760c53103c87c272f3d51f407
KanekoHato <1672019@maranatha.ac.id>
2021-10-07 10:54:47 +0700

    Export otomatis xml ke json
    Export otomatis xml ke json
    buang file tak terpakai

--- UpdateRRSAdmin.php
+++ UpdateRRSAdmin.php
@@ -1,34 +1,9 @@
 <?php
 include_once ('include/dbconnection.php');
+include_once ('include/berita.php');
 
-
-class Berita
-{
-    public function GetRss()
-    {
-        $curl = curl_init();
-
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => 'http://fetchrss.com/rss/615dc1e02861797479303c42615dc1b277531731d651a172.xml',
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'GET',
-        ));
-
-        $response = curl_exec($curl);
-
-        curl_close($curl);
-        return $response;
-    }
-}
-
-
-$tribunObj = new Berita();
-$rssData = $tribunObj->GetRss();
+$beritaObj = new Berita();
+$rssData = $beritaObj->GetRss();
 $xml = json_decode(json_encode(simplexml_load_string($rssData, null, LIBXML_NOCDATA)), true);
 $fp = fopen('berita.json', 'w');
 foreach ($xml['channel']['item'] as $data_berita) {
@@ -41,8 +16,7 @@ foreach ($xml['channel']['item'] as $data_berita) {
     $stmt = $dbh->prepare('INSERT INTO berita (judul,deskripsi,link,tanggal) VALUES(?,?,?,?)');  
     $stmt->execute([$judul,$deskripsi,$url_berita,$tanggal]);  
 }
-
-
+fclose($fp);
 echo 'Sukses Mengupdate Berita';
 
 
--- include/berita.php
+++ include/berita.php
@@ -1,12 +1,12 @@
 <?php
-class Tribun
+class Berita
 {
     public function GetRss()
     {
         $curl = curl_init();
 
         curl_setopt_array($curl, array(
-            CURLOPT_URL => 'https://www.tribunnews.com/rss',
+            CURLOPT_URL => 'http://fetchrss.com/rss/615dc1e02861797479303c42615dc1b277531731d651a172.xml',
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => '',
             CURLOPT_MAXREDIRS => 10,
@@ -22,3 +22,4 @@ class Tribun
         return $response;
     }
 }
+?>
